4890bf18ca11188ab3b4252983133520e0464732
Mindula Dilthushan Manamperi <minduladilthushan1@gmail.com>
2022-04-09 18:47:11 +0530

    project-init

---
+++ main.py
@@ -0,0 +1,10 @@
+# Project   : Red Alpha Assistant Bot
+# Author    : Mindula Dilthushan
+# Email     : minduladilthushan1@gmail.com
+# Date      : 22/04/09
+
+def project(project_name):
+    print(f'Hi, {project_name}')
+
+if __name__ == '__main__':
+    project('This is Red Alpha Assistant Bot Project')
